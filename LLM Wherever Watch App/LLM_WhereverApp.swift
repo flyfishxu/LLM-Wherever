150d73e9b021ef8bbb7129a5abf2f7a1c090fd6b
FlyfishXu <flyfishxu@outlook.com>
2025-06-29 17:12:20 +0800

    Initial Commit

---
+++ LLM Wherever Watch App/LLM_WhereverApp.swift
@@ -0,0 +1,17 @@
+//
+//  LLM_WhereverApp.swift
+//  LLM Wherever Watch App
+//
+//  Created by 徐义超 on 2025/6/29.
+//
+
+import SwiftUI
+
+@main
+struct LLM_Wherever_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LLM Wherever/LLM_WhereverApp.swift
@@ -0,0 +1,17 @@
+//
+//  LLM_WhereverApp.swift
+//  LLM Wherever
+//
+//  Created by 徐义超 on 2025/6/29.
+//
+
+import SwiftUI
+
+@main
+struct LLM_WhereverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
